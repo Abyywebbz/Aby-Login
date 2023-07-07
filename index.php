6cb996a39e711e68ccb3df1be5bd022a5eb663fb
Abyywebbz <138840221+Abyywebbz@users.noreply.github.com>
2023-07-07 11:01:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>- Abywebsite -</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Abywebsite</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">Prestasi</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-primary bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Selamat datang di website saya</h1>
+                <p class="lead">gimana kabar kalian guy!!</p>
+                <a class="btn btn-lg btn-light" href="#about">Les't Goo</a>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="about">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>About</h2>
+                        <p class="lead"> HALLO GUYS!! PERKENALKAN </p>
+                        <ul>
+                            <li>NAMA : Aby Rofiqy</li>
+                            <li>UMUR : 17 </li>
+                            <li>STATUS : Pelajar </li>
+                            <li>HOBBY : Sepakbola</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services section-->
+        <section class="bg-light" id="services">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>PRESTASI SAYA!!</h2>
+                        <p>
+                            <img src="assets/abyy.jpeg" />
+                          </p>
+                        <p class="lead">Saya mendapatkan Juara 3 dari lomba Classmetting di smkn 4 padalarang pada tangga 25 Agustus 2022.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Contact section-->
+        <section id="contact">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>Contact</h2>
+                        <p class="lead">INI CONTACT SAYA!!</p>
+                        <ul>
+                            <li>WHATSAPP : 082118634877</li>
+                            <li>INSTAGRAM : Abyyrofiqy</li>
+                            <li>TELEGRAM : Abyy Rofiqy </li>
+                            <li>EMAIL : abyrofiqy@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">abyrofiqy</p></div>
+        </footer>
+</html>
